e44284294c4b9354aedee916cc6549fa9f243461
Deniss <den.b@itservicesgroup.co.uk>
2017-02-09 12:28:57 +0000

    Add Handle real connections with Webmock
    Real connections were stubbed and processed
    accordingly, added context when appi is valid,
    and when it not.

--- app/services/youtube_connector_service.rb
+++ app/services/youtube_connector_service.rb
@@ -7,4 +7,25 @@ class YoutubeConnector
     @channel_id ||= "UCVguiojKA6iobcySMJ5boNA"
   end
 
+  def process
+    get_data(url)
+  end
+
+  private
+
+  def url
+    URI("https://www.googleapis.com/youtube/v3/search?key=#{api_key}&channelId
+              =#{channel_id}&part=snippet,id&order=date&maxResults=#{size}")
+  end
+
+  def error
+    {error: "bad data, possible you have no API key set"}
+  end
+
+  def get_data(url)
+    response = Net::HTTP.get_response(url)
+    return error unless response.code == "200"
+    JSON.parse(response.body).fetch("items")
+  end
+
 end
