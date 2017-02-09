29d74e5bc42b461b873e629e2b23b028fa258488
Deniss <den.b@itservicesgroup.co.uk>
2017-02-09 14:27:10 +0000

    Add #name to Rails.application

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,9 @@ Bundler.require(*Rails.groups)
 
 module YoutubeApi
   class Application < Rails::Application
+    def name
+      Rails.application.config.session_options[:key].sub(/^_/,'').sub(/_session/,'')
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
